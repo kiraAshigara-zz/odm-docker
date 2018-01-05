e49903ca9f9db80c1a0d69c5ae5794ea34798f56
alex <alexanderagudelo90@gmail.com>
2018-01-05 09:05:35 -0500

    readme

---
+++ builder/README.md
@@ -0,0 +1,14 @@
+# ODM Docker
+
+
+## Build Image
+for build the image execute the next command when the script ask for the odm path provide a valid one
+```bash
+sudo bash build-container.sh
+```
+
+## Run Container
+after run the build image step, we can execute the next command
+```bash
+bash start-container.sh
+```
--- builder/build-container.sh
+++ builder/build-container.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-sudo echo "127.0.0.1       localhost.integrichain.com" >> /etc/hosts
+cat jdka* > jdk-7u80-linux-x64.tar.gz
+echo "127.0.0.1       localhost.integrichain.com" >> /etc/hosts
 echo "path to dna-om"
 read path_dna_om
 docker build --no-cache -t integrichain .
 DOCKER_COMMAND="docker run -ti -p 8080:8080 -p 8081:8081 -v /tmp/order850_rt:/var/data/IN/order850_rt -v /tmp/ingrichain_m2:/root/.m2/ -v $path_dna_om:/dna-om integrichain bash /start.bash"
 echo $DOCKER_COMMAND > start-container.sh
 chmod a+x start-container.sh
+rm jdk-7u80-linux-x64.tar.gz
